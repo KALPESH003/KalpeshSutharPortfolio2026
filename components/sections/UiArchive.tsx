5eaf6672f82c22d1a54f4d4ffea7978a17e8c196
Kalpesh <sutharkamlesh003@gmail.com>
2026-09-20 00:07:52 +0530

    Updating

--- components/sections/UiArchive.tsx
+++ components/sections/UiArchive.tsx
@@ -11,7 +11,7 @@ const easePremium = [0.76, 0, 0.24, 1] as const;
 
 // --- MOCK LOCAL UI DESIGNS (Preview only needs the first 3) ---
 const uiDesigns = [
-  { id: "01", title: "Land Rover Dashboard", category: "System UI", type: "ui", src: "assets/images/UiArchive/RRui.png?auto=format&fit=crop&q=80&w=1600" },
+  { id: "01", title: "Land Rover Dashboard", category: "System UI", type: "ui", src: "assets/images/UiArchive/LandRoverPage.jpg?auto=format&fit=crop&q=80&w=1600" },
   { id: "02", title: "PocuhPal Expense Tracker", category: "USER Interface", type: "ui", src: "assets/images/UiArchive/pouchpal.png?auto=format&fit=crop&q=80&w=1600" }, 
   { id: "03", title: "Poster Design", category: "Asset", type: "Poster", src: "assets/images/UiArchive/Queen poster.png?auto=format&fit=crop&q=80&w=1600" },
 ];
